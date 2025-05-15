27c382b54744df54dcf4e2cb3a385635a638c471
MRM <mrm@gmail.com>
2025-05-15 14:49:12 +0530

    Enum First Commit

---
+++ src/main/java/org/example/oops/enumdemo/EnumDemo.java
@@ -0,0 +1,9 @@
+package org.example.oops.enumdemo;
+
+public class EnumDemo {
+    public static void main(String[] args) {
+        Day day=Day.MONDAY;
+        System.out.println(day);
+        day.display();
+    }
+}
