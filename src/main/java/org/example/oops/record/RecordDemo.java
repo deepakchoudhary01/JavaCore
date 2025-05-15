26705e0d5465269d20e0c27cd0927a4aa50e6028
MRM <mrm@gmail.com>
2025-05-15 14:51:09 +0530

    Record First Commit

---
+++ src/main/java/org/example/oops/record/RecordDemo.java
@@ -0,0 +1,8 @@
+package org.example.oops.record;
+
+public class RecordDemo {
+    public static void main(String[] args) {
+        var p=new Person(1, "Rahul", "Pune", "1234567890");
+        System.out.println(p);
+    }
+}
